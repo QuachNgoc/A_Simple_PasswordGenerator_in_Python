4eaa4089f435a54dd7bdcc2f5a9f6cc7624d5ffb
Ngoc <80575560+QuachNgoc@users.noreply.github.com>
2021-06-21 16:58:47 +0700

    Add files via upload

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -8,7 +8,7 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password = [" "]
+passw = [" "]
 for i in range(0,nr_letters):
     passw.append(letters[random.randint(0,len(letters)-1)])
 for i in range(0,nr_numbers):
@@ -17,8 +17,5 @@ for i in range(0,nr_symbols):
    passw.append(symbols [random.randint(0,len(symbols)-1)])
    
 passw.remove(" ")
-password = [" "]
-for i in range(0,len(passw)):
-    password.append( passw[random.randint(0,len(passw)-1)] )
-password.remove(" ")
-print("Your password is: " + ''.join(password))
+random.shuffle(passw)
+print("Your password is: " + ''.join(passw))
